2830a9560511ac5921ce47bfa9833a4e2cc080af
zeandrecarrieres <zeandredev@gmail.com>
2021-02-18 13:42:38 +0100

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar(propos) {
+    return (
+        <div>
+            <div className="navbar">
+                <div className="logo">JOSEPH <br></br> ANDRE</div>
+                <ul className="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                    
+                </ul>
+                   
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default[
+    {
+        id: 1,
+        name: "Thomas Le Bris",
+        attendance: true,
+    },
+    {
+        id: 2,
+        name: "Titouan Escorneboueu",
+        attendance: true,
+    },
+    {
+        id: 3,
+        name: "Charlotte Menière",
+        attendance: true,
+    },
+    {
+        id: 4,
+        name: "Julien PARDOEL",
+        attendance: true,
+    },
+    {
+        id: 5,
+        name: "Corentin Le Berre",
+        attendance: true,
+    },
+    {
+        id: 6,
+        name: "Sébastien Avenet",
+        attendance: true,
+    },
+    {
+        id: 7,
+        name: "Anthony Ringeisen",
+        attendance: true,
+    },
+    {
+        id: 8,
+        name: "Karim ",
+        attendance: true,
+    },
+    {
+        id: 9,
+        name: "Houria BENHASSENA",
+        attendance: true,
+    },
+    {
+        id: 10,
+        name: "Joseph Carrieres",
+        attendance: true,
+    },
+];
